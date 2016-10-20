73b9ea38d458f850ca3aa50bc9e1e913df06a708
Sandro R DeSouza <sandro.fisica@gmail.com>
2016-10-20 12:22:10 -0200

    Eliminei a dependência com o Pandas e enxuguei o código

--- TSL.py
+++ TSL.py
@@ -3,12 +3,11 @@
 # Teoria Secular Linear
 # =====================
 #
-# O objetivo deste programa é aplicar a Teoria Secular Linear a um exemplo
-# extraído de Carpino Teoria lineare delle perturbazioni secolari
-# (o texto está em italiano). O arquivo pode ser baixado
-# neste site: http://www.brera.mi.astro.it/~carpino/didattica/lagrange.pdf.
-#
+# O objetivo deste programa é aplicar a Teoria Secular Linear a um
+# planetario
+
 # Definições
+#      m: Massa
 #      a: Semieixo maior
 #      e: Excentricidade
 #      I: Inclinação
@@ -24,14 +23,11 @@
 # * Calcular os coeficientes das matrizes A e B
 # * Calcular os autovetores e autovalores das matrizes A e B
 # * Calcular os autovetores G_{ij} e S_{ij}
-# * Determinar as condições iniciais Gamma_{i}, eta_{i}, Sigma, nu
-#   a partir dos elementos médios
-
 
 # %% Importando módulos
 import sys
+import os.path
 import numpy as np
-import pandas as pd
 from scipy.integrate import quad
 
 # %% Definições de funções
@@ -72,25 +68,9 @@ def parte3(K, i, j, a):
     """
     return coeff_laplace(K, al(a[i], a[j]))
 
-# %% Função que adiciona título com sublinhado
-def titulo(str_titulo):
-    print('\n')
-    print(str_titulo)
-    print(len(str_titulo) * '-')
 
 # %% Função main - programa principal
-#def main():
 if __name__ == '__main__':
-    # %% Apresentação do programa
-
-    titulo('TSL - Teoria Secular Linear')
-
-    apresentacao =""" O objetivo deste programa é aplicar a Teoria Secular
-    Linear a um exemplo extraído de 'Carpino Teoria lineare delle perturbazioni
-    secolari' (o texto está em italiano). O arquivo pode ser obtido no endereço:
-    http://www.brera.mi.astro.it/~carpino/didattica/lagrange.pdf."""
-
-    print(apresentacao)
 
     # %% Constantes
     # Constante da gravitação universal
@@ -98,117 +78,37 @@ if __name__ == '__main__':
 
     # Massa do Sol
     M = 1.0 # Massa 'unitária' para o Sol
-
-    # %% Dados dos planetas
-
+    
     # Leitura dos dados
     input_file = sys.argv[1]
-    try:
-        pl = pd.read_fwf(input_file)
-    except:
-        print('Não foi possível ler o arquivo!')
-
+    
+    # Lendo nome dos planetas
+    planetas = np.genfromtxt(input_file, dtype='str', usecols=0, skip_header=1)
+    
+    # Lendo dados numéricos
+    pl = np.genfromtxt(input_file, dtype='float', usecols=(1, 2, 3, 4, 5), skip_header=1)
+    
     # Semieixo maior
-    a = pl['a'] # unidade AU
+    a = pl[:,1] # unidade AU
 
     # Comprimento do vetor de semieixos.
     len_a = len(a)
 
-    # Criando a coluna varpi
-    # Se a valor de varpi for superior a 360, subtrair 360 graus.
-    #pl['varpi'] = pl['capom'] + pl['omega']
-    #for i in pl.index:
-    #    if pl['varpi'][i] >=360:
-    #        pl.loc[i,'varpi'] = pl.loc[i,'varpi'] - 360
-
-    m = pl['m'] # Considerando massa unitária para o Sol
+    # Massa
+    m = pl[:,0] # Considerando massa unitária para o Sol
 
     # Dados dos movimentos médios n calculado
     #n = (G * (M + m)/a**3)**(1/2) # unidade rad/day
-    #pl['n'] = n
 
-    # Dados dos movimentos médios lidos
-    n = pl['n'] # deg/day
+    # Dados dos movimentos médios n lidos
+    n = pl[:,4] # deg/yr
     n = (n * np.pi / 180) / 365.25 # unidade rad/day
 
     # Excentricidade
-    e = pl['e']
+    e = pl[:,2]
 
     # Inclinação
-    inc = pl['inc'] # unidade deg
-
-    # Longitude do periastro
-    #varpi = pl['varpi'] # unidade deg
-
-    # Longitude do nodo ascendente
-    #capom = pl['capom'] # unidade
-
-
-    # %% Verificando dados de entrada
-
-    titulo('Dados de entrada')
-    print(pl[["Planeta", "m", "a", "e", "inc", "n"]])
-
-    titulo('Observação: Unidades')
-    print('    [m]: unidade de massa solar')
-    print('    [a]: AU')
-    print('  [inc]: deg')
-    #print('[capom]: deg')
-    #print('[omega]: deg')
-    print('    [n]: rad/day')
-    #print('[varpi]: deg')
-
-    #titulo('Observação: Data')
-    #print('Os ângulos foram tomados na data 28 de Julho de 1969,')
-    #print('no site do JPL da NASA, para ficar de acordo com')
-    #print('o texto de Carpino, na Tabela III, página 27.')
-
-    # %% Verificando função alpha
-    # A resultado pode ser verificado no exemplo de Carpino
-    # (http://www.brera.mi.astro.it/~carpino/didattica/lagrange.pdf),
-    # página 25. A diagonal não está definida e portanto foi
-    # atribuido o valor zero. Como era de se esperar, a matriz é diagonal.
-
-    #M_alpha = np.zeros((len_a, len_a))
-    #for i in range(0, len_a):
-    #    for j in range(0,len_a):
-    #        if j != i:
-    #            M_alpha[i, j] = al(a[i], a[j])
-    #        else:
-    #           M_alpha[i, j] = 0
-
-    #titulo('Função alfa (página 25)')
-    #print(M_alpha)
-
-    # %% Verificando função para os Coeficientes de Laplace
-    # b_{3/2}^{1}(\alpha_{i,j})$
-
-    #M_laplace = np.zeros((len_a, len_a))
-    #for i in range(0, len_a):
-    #    for j in range(0,len_a):
-    #        if j != i:
-    #            M_laplace[i, j] = coeff_laplace(1, al(a[i],a[j]))
-    #        else:
-    #            M_laplace[i, j] = 0
-    #titulo('Coeficientes de Laplace para k = 1 (página 25)')
-    #print(M_laplace)
-
-    # %% Verificando função para os Coeficientes de Laplace
-    # $b_{3/2}^{2}(\alpha_{i,j})$. O resultado pode ser verificado no exemplo
-    # de Carpino (http://www.brera.mi.astro.it/~carpino/didattica/lagrange.pdf)
-    # página 26. A diagonal não está definida e portanto foi atribuido o
-    # valor zero. Como era de se esperar, a matriz é diagonal.
-
-    #M_laplace = np.zeros((len_a, len_a))
-    #for i in range(0, len_a):
-    #    for j in range(0,len_a):
-    #        if j != i:
-    #            M_laplace[i, j] = coeff_laplace(2, al(a[i],a[j]))
-    #        else:
-    #            M_laplace[i, j] = 0
-
-    #titulo('Coeficientes de Laplace para k = 2 (página 26)')
-    #print(M_laplace)
+    inc = pl[:,3] # unidade deg
 
     # %% Determinando a matriz A e B
     # Matriz A
@@ -287,26 +187,19 @@ if __name__ == '__main__':
     B_sim_eigenValues = B_sim_eigenValues[idx]
     B_sim_eigenVectors = B_sim_eigenVectors[:,idx]
     
-    # %% Resultados da matriz A
-    # A matriz simétrica, na página 26 do texto de Carpino.
-    
     # Salvando no arquivo
     output_file = 'resultados-' + str(input_file)
 
     f = open(output_file, 'w+')
-
-    titulo('Resultado da matriz A em deg/yr')
     
-    f.write('Resultado da matriz A em deg/yr\n')
-    f.write('\nMatriz A associada a Excentricidade\n')
+    f.write('\nMatriz A associada a Excentricidade em deg/yr\n')
     f.write(str(A))
     f.write('\nAutovalores de A\n')
     f.write(str(A_eigenValues))
     f.write('\nAutovetores de A\n')
     f.write(str(A_eigenVectors))
     
-    f.write('Resultado da matriz B em deg/yr\n')
-    f.write('\nMatriz B associada a Excentricidade\n')
+    f.write('\nMatriz B associada a Inclinação em deg/yr\n')
     f.write(str(B))
     f.write('\nAutovalores de B\n')
     f.write(str(A_eigenValues))
@@ -315,42 +208,19 @@ if __name__ == '__main__':
 
     f.close()
     
-    print('Matriz A associada a Excentricidade')
-    print(A)
-    print('\n Autovalores de A')
-    print(A_eigenValues)
-    print('\n Autovetores de A')
-    print(A_eigenVectors)
-    #print('\n Matriz A simétrica, conforme Carpino, página 26')
-    #print(A_cal)
-    #print('\n Autovalores de A simetrica')
-    #print(A_sim_eigenValues)
-    #print('\n Autovetores de A simetrica')
-    #print(A_sim_eigenVectors)
-
-    # %% Resultados da matriz B
-    # A matriz simétrica, na página 26 do texto de Carpino.
-
-    titulo('Resultado da matriz B em arcsec/yr')
+    # Informação ao usuário
+    os.system('clear')
+    print("TSL - Teoria Secular Linear")
+    print("---------------------------")
     
-    print('Matriz B associada a Inclinação')
-    print(B)
-    print('\n Autovalores de B')
-    print(B_eigenValues)
-    print('\n Autovetores de B')
-    print(B_eigenVectors)
-    #print('\n Matriz B simétrica, conforme Carpino, página 27')
-    #print(B_cal)
-    #print('\n Autovalores de B simetrica')
-    #print(B_sim_eigenValues)
-    #print('\n Autovetores de B simetrica')
-    #print(B_sim_eigenVectors)
+    try:
+         os.path.isfile(output_file)
+         print()
+         print('O resultado foi gravado no arquivo', output_file)
+         print()
+    except:
+        print('O arquivo não foi criado')
+        
     
-    # %% Criando novas colunas com as variáveis regularizadas
-    #pl['h'] = e * np.sin(np.radians(varpi))
-    #pl['k'] = e * np.cos(np.radians(varpi))
-    #pl['p'] = np.sin(np.radians(inc)) * np.sin(np.radians(capom))
-    #pl['q'] = np.sin(np.radians(inc)) * np.cos(np.radians(capom))
+   
 
-    #titulo('Variáveis regulares')
-    #print(pl[['Planeta', 'h', 'k', 'p', 'q']])
